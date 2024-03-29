777ef4ce6a3e70c445995e806133937011d51d07
ss23094527 <ss23094527@gmail.com>
2024-03-29 22:00:21 +0800

    　幻燈片需再修正

--- src/component/Carousel.js
+++ src/component/Carousel.js
@@ -1,15 +1,17 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { View, StyleSheet, ScrollView, Image, Dimensions, TouchableOpacity, Text } from 'react-native';
 import carouselData from '../../src/json/carouselData.json'; 
+import { AntDesign } from '@expo/vector-icons';
 
 const { width } = Dimensions.get('window');
 
 const Carousel = () => {
   const scrollViewRef = useRef();
 
-  const handleScroll = (event) => {
-    const contentOffsetX = event.nativeEvent.contentOffset.x;
-  };
+  useEffect(() => {
+    const interval = setInterval(handlePressNext, 3000); // 自動輪播間隔為 3 秒
+    return () => clearInterval(interval); // 清除定時器
+  }, []);
 
   const handlePressNext = () => {
     scrollViewRef.current.scrollTo({ x: width, animated: true });
@@ -26,10 +28,9 @@ const Carousel = () => {
         horizontal
         pagingEnabled
         showsHorizontalScrollIndicator={false}
-        onScroll={handleScroll}
         scrollEventThrottle={16}
       >
-        {/* 在這里渲染輪播項 */}
+        {/* 渲染輪播項 */}
         {carouselData.map((item, index) => (
           <View key={index} style={styles.carouselItem}>
             <Image source={{ uri: item.image }} style={styles.image} />
@@ -38,10 +39,10 @@ const Carousel = () => {
       </ScrollView>
       {/* 添加前進和後退按鈕 */}
       <TouchableOpacity style={[styles.navButton, styles.prevButton]} onPress={handlePressPrev}>
-        <Text style={styles.navButtonText}>Prev</Text>
+        <AntDesign name="left" size={24} color="white" />
       </TouchableOpacity>
       <TouchableOpacity style={[styles.navButton, styles.nextButton]} onPress={handlePressNext}>
-        <Text style={styles.navButtonText}>Next</Text>
+        <AntDesign name="right" size={24} color="white" />
       </TouchableOpacity>
     </View>
   );
@@ -51,14 +52,14 @@ const styles = StyleSheet.create({
   carouselContainer: {
     position: 'relative',
     width: '100%',
-    height: 200,
+    marginTop: 20,
+    height: 300,
   },
   carouselItem: {
     width,
     height: 200,
   },
   image: {
-    zIndex:1,
     width: '100%',
     height: '100%',
     resizeMode: 'cover',
@@ -67,8 +68,8 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: '50%',
     paddingHorizontal: 20,
-    paddingVertical: 10,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    paddingVertical: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0)',
     borderRadius: 5,
     zIndex: 1,
   },
