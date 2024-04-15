dd3d83816a59a5368aa944a1d7025b02b2c38ac6
ss23094527 <ss23094527@gmail.com>
2024-04-15 18:32:33 +0800

    detail-v02

--- App/Screen/DetailScreen.js
+++ App/Screen/DetailScreen.js
@@ -1,28 +1,79 @@
-import * as React from 'react';
-import { View, StyleSheet, TouchableOpacity, Text, Image ,Button} from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text, Image, Button } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { COLORS } from '../../theme';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native'; // 添加 useRoute
 import { Video, ResizeMode } from 'expo-av';
-
+import StarList from '../../src/component/StarList';
+import { selectCounter, selectColorMode } from "../../src/redux/counterSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import { Ionicons } from '@expo/vector-icons';
 
 const DetailScreen = () => {
-
     const navigation = useNavigation();
- const [showPlayButton, setShowPlayButton] = React.useState(true);
+    const route = useRoute();
+
+    const [showPlayButton, setShowPlayButton] = React.useState(true);
     const [isPlaying, setIsPlaying] = React.useState(false);
+    const colorMode = useSelector(selectColorMode);
+    const dispatch = useDispatch();
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [activeSegment, setActiveSegment] = React.useState('introduction'); // Add activeSegment state
+
+    const title = route.params.title;
+    const author = route.params.author;
+    const price = route.params.price;
+    const image = route.params.image;
+    const stars = route.params.stars;
+    const comment = route.params.comment;
+
+    // Function to render different content based on activeSegment value
+    function renderVideoContent(activeSegment) {
+        switch (activeSegment) {
+            case 'introduction':
+                return (
+                    <View style={styles.segmentcontainer}>
+                        <Text style={styles.contentTitle}>課程介紹</Text>
+                        <Text style={styles.contentText}>這是一個關於課程介紹的內容。</Text>
+                    </View>
+                );
+            case 'catalog':
+                return (
+                    <View style={styles.segmentcontainer}>
+                        <Text style={styles.contentTitle}>課程目錄</Text>
+                        <Text style={styles.contentText}>這是一個關於課程目錄的內容。</Text>
+                    </View>
+                );
+            case 'comment':
+                return (
+                    <View style={styles.container}>
+                        <Text style={styles.contentTitle}>課程評論</Text>
+                        <Text style={styles.contentText}>這是一個關於課程評論的內容。</Text>
+                    </View>
+                );
+            case 'material':
+                return (
+                    <View style={styles.container}>
+                        <Text style={styles.contentTitle}>課程素材</Text>
+                        <Text style={styles.contentText}>這是一個關於課程素材的內容。</Text>
+                    </View>
+                );
+            default:
+                return null;
+        }
+    }
+
     // Header
     function renderHeader() {
-        const [activeSegment, setActiveSegment] = React.useState('introduction');
-
         const handleSegmentPress = (segment) => {
             setActiveSegment(segment);
+            console.log('Active Segment:', segment); // 新增 log 語句
         };
-
+    
         return (
             <View style={styles.header}>
                 <TouchableOpacity onPress={() => navigation.navigate('TabNavigation')} style={styles.backButton}>
-                    <AntDesign name="arrowleft" size={24} color={COLORS.black} />
+                    <Ionicons name="chevron-back" size={24} color={COLORS.black} />
                 </TouchableOpacity>
                 <View style={styles.segmentedControl}>
                     <TouchableOpacity
@@ -36,14 +87,14 @@ const DetailScreen = () => {
                         <Text style={[styles.segmentText, activeSegment === 'catalog' && styles.activeSegmentText]}>目錄</Text>
                     </TouchableOpacity>
                     <TouchableOpacity
-                        style={[styles.segment, activeSegment === 'comments' && styles.activeSegment]}
-                        onPress={() => handleSegmentPress('comments')}>
-                        <Text style={[styles.segmentText, activeSegment === 'comments' && styles.activeSegmentText]}>評論</Text>
+                        style={[styles.segment, activeSegment === 'comment' && styles.activeSegment]}
+                        onPress={() => handleSegmentPress('comment')}>
+                        <Text style={[styles.segmentText, activeSegment === 'comment' && styles.activeSegmentText]}>評論</Text>
                     </TouchableOpacity>
                     <TouchableOpacity
-                        style={[styles.segment, activeSegment === 'materials' && styles.activeSegment]}
-                        onPress={() => handleSegmentPress('materials')}>
-                        <Text style={[styles.segmentText, activeSegment === 'materials' && styles.activeSegmentText]}>素材</Text>
+                        style={[styles.segment, activeSegment === 'material' && styles.activeSegment]}
+                        onPress={() => handleSegmentPress('material')}>
+                        <Text style={[styles.segmentText, activeSegment === 'material' && styles.activeSegmentText]}>素材</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -51,7 +102,6 @@ const DetailScreen = () => {
     }
 
     // Video
-   
     function renderVideo() {
         const video = React.useRef(null);
         const [status, setStatus] = React.useState({});
@@ -84,14 +134,21 @@ const DetailScreen = () => {
                     </TouchableOpacity>
                 )}
                 <View style={styles.videoInfo}>
-                    <Text style={styles.videoTitle}>影片標題</Text>
-                    <Text style={styles.videoAuthor}>作者名稱</Text>
-                    <Text style={styles.videoRating}>評分: 4.5/5</Text>
+                    <Text style={styles.videoTitle}>{title}</Text>
+                    <Text style={styles.videoAuthor}>{author}</Text>
+                    <View style={styles.ratingContainer}>
+                        <StarList stars={stars} />
+                        <Text style={[styles.comment, { color: colorMode === 'dark' ? COLORS.white : COLORS.black }]}>{comment}</Text>
+                    </View>
+                    <Text style={[styles.price, { color: colorMode === 'dark' ? COLORS.white : COLORS.primary }]}>{price}</Text>
+                    <View style={styles.separator}></View>
+                   
+                    {renderVideoContent(activeSegment)}
                 </View>
+                
             </View>
         );
     }
-
 
     return (
         <View style={styles.container}>
@@ -104,25 +161,26 @@ const DetailScreen = () => {
 };
 
 const styles = StyleSheet.create({
+    segmentcontainer:{
+        
+    },
     container: {
         flex: 1,
-   marginTop:20,
-       
+        marginTop: 20,
     },
     video: {
         alignSelf: 'center',
         width: 450,
         height: 200,
-      },
-      playButton: {
+    },
+    playButton: {
         position: 'absolute',
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
         borderRadius: 100,
-       
         justifyContent: 'center',
         alignItems: 'center',
-        top:'20%',
-        left:'42%',
+        top: '15%',
+        left: '42%',
         width: 80,
         height: 80,
     },
@@ -142,13 +200,14 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         flex: 1,
     },
-  
     segment: {
         paddingVertical: 10,
-        paddingHorizontal: 15,
+        paddingHorizontal: 20,
     },
     segmentText: {
+        fontSize: 17,
         color: COLORS.black,
+        fontWeight: 'bold',
     },
     activeSegment: {
         borderBottomWidth: 2,
@@ -162,12 +221,11 @@ const styles = StyleSheet.create({
         marginTop: 0,
         paddingHorizontal: 20,
     },
-  
     videoInfo: {
         marginTop: 10,
     },
     videoTitle: {
-        fontSize: 18,
+        fontSize: 20,
         fontWeight: 'bold',
     },
     videoAuthor: {
@@ -179,6 +237,44 @@ const styles = StyleSheet.create({
         color: COLORS.black,
         marginTop: 5,
     },
+    ratingContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 5,
+    },
+    comment: {
+        marginLeft: 5,
+        fontSize: 12,
+        color: COLORS.gray,
+    },
+    price: {
+        flexWrap: 'wrap',
+        fontSize: 23,
+        fontWeight:'bold',
+        color: COLORS.black,
+        marginBottom: 5,
+    },
+    separator: {
+        borderRadius: 30,
+        width: '100%',
+        height: 1,
+        backgroundColor: COLORS.lightGray,
+        marginTop: 10,
+    },
+    Title:{
+        fontSize:100,
+        marginTop:200,
+    },
+    contentTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    contentText: {
+        fontSize: 16,
+        lineHeight: 24,
+    },
 });
 
 export default DetailScreen;
+
